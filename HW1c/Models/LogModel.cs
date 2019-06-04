56ce467fbd7ced7bda9a8f324acc66dd204e6ff2
Phong Nguyen <nguyenphong@seattleu.edu>
2019-06-04 01:37:49 -0700

    Add project files.

---
+++ HW1c/Models/LogModel.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace HW1c.Models
+{
+    public class LogModel
+    {
+        // The ID of the record
+        public string ID { get; set; } = Guid.NewGuid().ToString();
+
+        // The Event Type
+        // Todo, change to an Enum
+        [Display(Name="Event Type", Description = "Type of Event")]
+        public int EventType { get; set; }
+
+        // The Value Sent by the Phone, as a string
+        public string Value { get; set; }
+
+        // Recorded Date Time at the Server
+        public DateTime RecordedDateTime { get; set; } = DateTime.Now;
+
+        // ID of the Phone as a quid
+        public string PhoneID { get; set; }
+
+        // App Version string
+        public string AppVersion { get; set; }
+
+        /// <summary>
+        /// Constructor for Log Model
+        /// Calls to Initialize to set initial settings
+        /// </summary>
+        public LogModel()
+        {
+            Initialize();
+        }
+
+        /// <summary>
+        /// Intialize
+        /// Sets default values, such as DateTime as needed by the system
+        /// </summary>
+        public void Initialize()
+        {
+            // None right now...
+        }
+
+        /// <summary>
+        /// Update fields passed in
+        /// Updates all fields to be the values passed in
+        /// Does NOT update the ID field, this allows for the method to be used as part of a copy.
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public bool Update(LogModel data)
+        {
+            if (data == null)
+            {
+                return false;
+            }
+
+            // Don't update the ID, leave the old on in place
+            ID = data.ID;
+
+            // Update all the other fields
+            EventType = data.EventType;
+            Value = data.Value;
+            RecordedDateTime = data.RecordedDateTime;
+            PhoneID = data.PhoneID;
+            AppVersion = AppVersion;
+
+            return true;
+        }
+    }
+}
---
+++ UnitTests/Models/LogModelTests.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HW1c.Models;
+
+namespace UnitTests.Models
+{
+    [TestClass]
+    public class LogModelUnitTests
+    {
+        [TestMethod]
+        public void LogModel_Default_Should_Pass()
+        {
+
+            // Arange
+
+            // Act
+            var result = new LogModel();
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void LogModel_Update_Valid_Data_Should_Pass()
+        {
+
+            // Arange
+            var myTest = new LogModel();
+            var myUpdate = new LogModel
+            {
+                PhoneID = "Phone id"
+            };
+
+            // Act
+            var result = myTest.Update(myUpdate);
+
+            // Assert
+            Assert.AreEqual(myUpdate.PhoneID, myTest.PhoneID);
+        }
+
+        [TestMethod]
+        public void LogModel_Update_Invalid_Null_Data_Should_Fail()
+        {
+
+            // Arange
+            var myTest = new LogModel(); 
+
+            // Act
+            var result = myTest.Update(null);
+
+            // Assert
+            Assert.AreEqual("abc", myTest.PhoneID);
+        }
+
+        [TestMethod]
+        public void LogModel_Update_Different_ID_Should_Not_Update()
+        {
+            // Business rule is that the ID is not updatable.  
+            // So need to add a test to verify that the ID does not change
+            // If the code has a bug, fix the bug and enable the test assert
+            
+            // Arange
+            var myTest = new LogModel();
+            var myUpdate = new LogModel
+            {
+                PhoneID = "Phone id",
+                ID = "bogus"
+            };
+
+            // Remeber the old ID
+            var myTestID = myTest.ID;
+
+            // Act
+            var result = myTest.Update(myUpdate);
+
+            // Assert
+            //Assert.AreEqual(myTestID, myTest.ID);
+        }
+    }
+}
